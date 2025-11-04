b9216123fccdcabef3dbe301a83c24d6cb7a2c45
ManishaKhatri-class <codeblogger17@gmail.com>
2025-11-04 14:51:25 +0530

    Post Delete Added

--- blog/urls.py
+++ blog/urls.py
@@ -2,7 +2,7 @@
 from django.contrib import admin
 from django.urls import path
 from blog import views
-from .views import (PostListView,PostDetailView,PostCreateView,PostUpdateView)
+from .views import (PostListView,PostDetailView,PostCreateView,PostUpdateView,PostDeleteView)
 from django.conf import settings
 from django.conf.urls.static import static
 urlpatterns = [
@@ -10,6 +10,7 @@ urlpatterns = [
     path('post/<int:pk>/', PostDetailView.as_view(),name='post-detail'),#show post by primary key int
      path('post/new/', PostCreateView.as_view(),name='post-create'),#to create new post
       path('post/<int:pk>/update/', PostUpdateView.as_view(),name='post-update'),#to update new post
+       path('post/<int:pk>/delete/', PostDeleteView.as_view(),name='post-delete'),#to delete post
     path('about/',views.about,name='blog-about')
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 #create post_list.html in templates in views.py 
--- blog/views.py
+++ blog/views.py
@@ -1,11 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Post
-from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.mixins import LoginRequiredMixin,UserPassesTestMixin
 from django.views.generic import (ListView,
 DetailView,
 CreateView,
-UpdateView
+UpdateView,
+DeleteView
 )
 #create home function to manage resquests
 #create list
@@ -27,6 +28,7 @@ class PostDetailView(DetailView):
 	model =Post
 	
 class PostCreateView(LoginRequiredMixin,CreateView):
+#mixin so only user of blog shoes post is can only update his post
 	model =Post
 	fields=['title','content']
 	#create currently loginned user the author
@@ -34,13 +36,27 @@ class PostCreateView(LoginRequiredMixin,CreateView):
 		form.instance.author=self.request.user 
 		return super().form_valid(form)
 
-class PostUpdateView(LoginRequiredMixin,UpdateView):
+class PostUpdateView(LoginRequiredMixin,UserPassesTestMixin,UpdateView):
 	model =Post
 	fields=['title','content']
 	#create currently loginned user the author
 	def form_valid(self,form):
 		form.instance.author=self.request.user 
 		return super().form_valid(form)
+	def test_func(self):
+		post=self.get_object()
+		if self.request.user ==post.author:
+			return True
+		return False
+
+class PostDeleteView(LoginRequiredMixin,UserPassesTestMixin,DeleteView):
+	model =Post
+	
+	def test_func(self):
+		post=self.get_object()
+		if self.request.user ==post.author:
+			return True
+		return False
 
 
 def about(request):
